576cd056e0721e5d8a6b5f9f672c08fbcbb3059f
Varun Ramachandran <varun.rama@gmail.com>
2017-04-01 12:47:06 -0400

    w2p

--- app/src/main/java/com/example/varun/firemesh/MainActivity.java
+++ app/src/main/java/com/example/varun/firemesh/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.varun.firemesh;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private final IntentFilter intentFilter = new IntentFilter();
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    BroadcastReceiver mReceiver;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new BReceiver(mManager, mChannel, this);
+        (findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast toast = Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT);
+                        toast.show();
+                        mManager.requestPeers(mChannel, new WifiP2pManager.PeerListListener () {
+                            @Override
+                            public void onPeersAvailable(WifiP2pDeviceList peers) {
+                                Toast toast = Toast.makeText(getApplicationContext(), "peers: " + peers.toString(), Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+                        });
+                    }
+                    @Override
+                    public void onFailure(int reasonCode) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "error: " + reasonCode, Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, intentFilter);
+    }
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+
+
 }
